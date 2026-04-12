e89fd21d81989b1df4b76e3d71eb63989b67705e
Prashant Kumar Updhyay <prashantupadhyay149@gmail.com>
2026-04-12 16:24:02 +0530

    disable migration for render

--- Program.cs
+++ Program.cs
@@ -21,11 +21,11 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
 // AUTO MIGRATION
-using (var scope = app.Services.CreateScope())
+/*using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
-}
+}*/
 
 // Swagger (ENABLE ALWAYS)
 app.UseSwagger();
